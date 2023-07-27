8b3587f17f7edc789b9c25b6a6c048ce3bfdc25c
ThiaudioTT <thiago.sky25@gmail.com>
2023-07-27 17:32:10 -0300

    feat: created some structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoinputComponent } from './components/videoinput/videoinput.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoinputComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/videoinput/videoinput.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-videoinput',
+  templateUrl: './videoinput.component.html',
+  styleUrls: ['./videoinput.component.scss']
+})
+export class VideoinputComponent {
+
+}
